d279d21ad68351bc316fdab589c9ed0a0cbb01a5
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-12-20 20:09:09 +0300

    content progress

--- src/pages/Content/Content.jsx
+++ src/pages/Content/Content.jsx
@@ -12,13 +12,18 @@ export default function Content() {
         <div>
         <section className={sectionClassName}>
             <div className='-top-[140px] relative h-screen -z-10'>
+                <div className="h-screen">
                 <img 
                 className="content-image" 
-                src="https://pbs.twimg.com/media/F-5gPXlXwAEzB7V?format=jpg&name=large" 
+                src="https://pbs.twimg.com/media/F-5gPXlXwAEzB7V?format=jpg&name=large"
                 alt=""/>
-                <p className="text-[50px] absolute top-[140px] text-white font-bold uppercase">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae praesentium ab mollitia asperiores! A quidem soluta earum animi, minus possimus suscipit cupiditate minima ea eum impedit labore illo sapiente ut reprehenderit porro beatae explicabo molestiae modi. Hic perspiciatis sequi maiores?
+                </div>
+                <div className=" absolute ml-20 mt-20 h-[500px] flex text-center items-center w-1/3 top-[140px] text-white font-bold uppercase">
+                <p 
+                className="lg:text-[40px] text-[30px] text-shadow-xl ">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae praesentium ab 
                 </p>
+                </div>
             </div>
             <div className='text-[50px] underline uppercase font-bold'>hello world!</div>
             <div className='text-[50px] underline uppercase font-bold'>hello world!</div>
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -3,7 +3,8 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setScrollY } from '../../body/bodySlice';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
 
 
 export default function Navbar() {
@@ -28,19 +29,22 @@ useEffect(() => {
 }, [dispatch]);
 
     return (
-        <nav className={ ` shadow-xl z-10 transition-all duration-500 ease-in-out font-base mx-16 navbar-bg h-[100px] flex justify-around items-center ${scrollY > 0 ? 'fixed h-[80px] top-0 mx-auto rounded-tl-none w-full':'rounded-tl-full mt-10'}`}>
+        <nav className={ ` shadow-xl z-10 lg:rounded-none transition-all duration-500 ease-in-out font-base lg:mx-16 mx-0 navbar-bg xl:h-[100px] flex justify-around items-center ${scrollY > 0 ? 'fixed h-[80px] top-0 lg:mx-auto mx-0 rounded-tl-none w-full':'lg:rounded-tl-full rounded-none lg:mt-10 mt-0'}`}>
             <div>
                 <img className="h-5 w-5 bg-white" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo" />
             </div>
-            <div className="text-white cursor-pointer bg-red-500 flex justify-end gap-10">
-                <Link to="#">Anasayfa</Link>
-                <Link to="#">Referanslar</Link>
-                <Link to="#">Anasayfa</Link>
-                <Link to="#">Anasayfa</Link>
-                <Link to="#">Hakkımızda</Link>
+            <div className="text-white cursor-pointer w-3/5  hidden lg:flex justify-end gap-10">
+                <NavLink to="#">Anasayfa</NavLink>
+                <NavLink to="#">Referanslar</NavLink>
+                <NavLink to="#">Anasayfa</NavLink>
+                <NavLink to="#">Anasayfa</NavLink>
+                <NavLink to="#">Hakkımızda</NavLink>
             </div>
-            <div>
-                <p className="text-white">search</p>
+            <div className='flex bg-transparent '>
+              <label htmlFor="search" className='flex'>
+                <FaSearch name='search' className='text-white cursor-pointer h-full mx-1' />
+                <input type='text' name='search' placeholder='Ara' className="text-white w-40 p-1 border-b bg-transparent border-white focus:border-red-600 transition-colors ease-in-out"/>
+              </label>
             </div>
         </nav>
     )
